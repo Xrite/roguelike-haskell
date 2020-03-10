92bd4aa44b12f27107167281590176ab59a0f43c
Your Name <you@example.com>
2020-03-11 01:14:39 +0300

    add Unit

---
+++ src/Game/Unit.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Game.Unit
+  ( UnitData,
+    stats,
+    timedEffects,
+    inventory,
+    baseWeapon,
+    Unit (..),
+    AnyUnit,
+    packUnit,
+  )
+where
+
+import Control.Lens
+import Game.Effect
+import Game.Inventory
+import Game.Item
+import Game.Stats
+import Game.TimedEffects
+
+data UnitData
+  = UnitData
+      { _stats :: Stats,
+        _timedEffects :: TimedEffects,
+        _inventory :: Inventory,
+        _baseWeapon :: WeaponItem
+      }
+
+makeLenses ''UnitData
+
+-- | Something that can hit and run
+class Unit a where
+  asUnitData :: a -> UnitData
+  applyEffect :: Effect () -> a -> a
+
+data AnyUnit = forall a. Unit a => AnyUnit a
+
+packUnit :: Unit a => a -> AnyUnit
+packUnit unit = AnyUnit unit
