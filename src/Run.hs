c1ab738b35d6143594ff94e8e96e9203cf110353
Vladilav Yepifanov <evn1999@yandex.ru>
2020-03-15 16:26:06 +0300

    changed Run.hs so that it print a generated lvl

--- src/Run.hs
+++ src/Run.hs
@@ -11,7 +11,7 @@ run :: RIO App ()
 run = do
   logInfo $ displayShow $ runState (generateLevel param s) gen
   where
-    param = GeneratorParameters 20 1.7 5
+    param = makeGeneratorParameters 20 1.7 5
     s = Space (Coord 0 0) (Coord 100 100)
     gen = mkStdGen 42
   -- logInfo "We're inside the application!"
