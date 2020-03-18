eb92925846fd1ee5679699d38806b487580f5c89
Your Name <you@example.com>
2020-03-18 23:34:27 +0300

    tmp

--- src/Game.hs
+++ src/Game.hs
@@ -30,10 +30,22 @@ instance HasUI GameState where
     currentUI Game env = gameUI
 
 gameUI :: Environment -> UI GameState GameState
-gameUI gameEnv = makeGameUI . mkGameUI $ do
+gameUI gameEnv = makeGameUI . mkGameUI
+  $ do
     let level = getCurrentLevel $ gameEnv ^. _env
     setMap $ renderLevel level
     setArrowPress arrowPress
+  where
+    arrowPress :: Arrows -> GameState -> GameState
+    arrowPress Keys.Up (GameEnv env) = GameEnv . fromMaybe env
+      $ maybeMakeAction (UnitId 0) (Move Positive Zero) env
+    arrowPress Keys.Down (GameEnv env) = GameEnv . fromMaybe env
+      $ maybeMakeAction (UnitId 0) (Move Negative Zero) env
+    arrowPress Keys.Left (GameEnv env) = GameEnv . fromMaybe env
+      $ maybeMakeAction (UnitId 0) (Move Zero Positive) env
+    arrowPress Keys.Right (GameEnv env) = GameEnv . fromMaybe env
+      $ maybeMakeAction (UnitId 0) (Move Zero Negative) env
+
 
 renderLevel :: GameLevel -> [String]
 renderLevel lvl =
@@ -41,14 +53,3 @@ renderLevel lvl =
   where
     mp = lvl ^. lvlMap
     ((xFrom, yFrom), (xTo, yTo)) = getMapSize mp
-
-arrowPress :: Arrows -> GameEnv -> GameEnv
-arrowPress Keys.Up (GameEnv env) = GameEnv . fromMaybe env $ maybeMakeAction (UnitId 0) (Move Positive Zero) env
-arrowPress Keys.Down (GameEnv env) = GameEnv . fromMaybe env $ maybeMakeAction (UnitId 0) (Move Negative Zero) env
-arrowPress Keys.Left (GameEnv env) = GameEnv . fromMaybe env $ maybeMakeAction (UnitId 0) (Move Zero Positive) env
-arrowPress Keys.Right (GameEnv env) = GameEnv . fromMaybe env $ maybeMakeAction (UnitId 0) (Move Zero Negative) env
-
---currentEnv = __currentUI
-
---updateUI :: UI a -> Game a -> Game a
---updateUI = set _currentUI
