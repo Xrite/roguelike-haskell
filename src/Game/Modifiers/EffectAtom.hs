8f2094002fd523036afe9b5f91fad3cf8cc18ed1
Vladilav Yepifanov <evn1999@yandex.ru>
2020-04-19 00:23:15 +0300

    comments for EffectAtom

--- src/Game/Modifiers/EffectAtom.hs
+++ src/Game/Modifiers/EffectAtom.hs
@@ -8,21 +8,33 @@ module Game.Modifiers.EffectAtom
   )
 where
 
-data EffectAtom = Damage NonNegative
-                | Heal NonNegative
-                | GiveExp NonNegative
+-- |Describes basic effects that might be applied to a unit as part of game mechanics.
+data EffectAtom = Damage NonNegative -- ^ Deal damage to a unit
+                | Heal NonNegative -- ^ Heal a unit
+                | GiveExp NonNegative -- ^ Give experience points to a unit
 
+-- |A non negative Int.
 newtype NonNegative = NonNegative Int
 
+-- |Constructs a 'NonNegative' if provided value is not negative or throws an error otherwise.
 nonNegative :: Int -> NonNegative
 nonNegative n =
   if n >= 0
     then NonNegative n
     else error "Negative int in non-negative context"
 
+-- |Extracts value from 'NonNegative'
 fromNonNegative :: NonNegative -> Int
 fromNonNegative (NonNegative n) = n
 
+-- |Constructs a damaging 'EffectAtom'
+damage :: Int -> EffectAtom
 damage = Damage . nonNegative
+
+-- |Constructs a healing 'EffectAtom'
+heal :: Int -> EffectAtom
 heal = Heal . nonNegative
+
+-- |Constructs an experience giving 'EffectAtom'
+giveExp :: Int -> EffectAtom
 giveExp = GiveExp . nonNegative
