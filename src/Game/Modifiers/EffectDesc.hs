d746925c8c9fcb580ed9032e8ed15bacec65c675
Vladilav Yepifanov <evn1999@yandex.ru>
2020-04-19 00:30:47 +0300

    Comments for EffectDesc

--- src/Game/Modifiers/EffectDesc.hs
+++ src/Game/Modifiers/EffectDesc.hs
@@ -4,27 +4,34 @@ module Game.Modifiers.EffectDesc
   ( UnitOpKey
   , EffectDescM
   , EffectDesc
-  , EffectDescDSL (..)
+  , EffectDescDSL(..)
   , effectAtom
   , effectTypical
-  )
-where
+  ) where
 
+import Control.Monad.Free (Free(..))
 import Game.Modifiers.EffectAtom
-import Control.Monad.Free (Free (..))
 
+-- |A reference to a default 'UnitOp'
 type UnitOpKey = String
 
 -- TODO add timed modifier here? or to atom?
-data EffectDescDSL a = Atom EffectAtom a
-                | TypicalUnitOp UnitOpKey a
-                deriving (Functor)
+-- |DSL for describing effects.
+data EffectDescDSL a
+  = Atom EffectAtom a         -- ^ basic effect atom
+  | TypicalUnitOp UnitOpKey a -- ^ default effect reference that will be substituted by 'UnitOpFactory' 
+  deriving (Functor)
 
+-- |A monad for describing effects
 type EffectDescM a = Free EffectDescDSL a
+
+-- |Usual parametrization of 'EffectDescM' 
 type EffectDesc = EffectDescM ()
 
+-- |Constructs an 'EffectDesc' for applying an 'EffectAtom'
 effectAtom :: EffectAtom -> EffectDesc
 effectAtom atom = Free $ Atom atom $ Pure ()
 
+-- |Constructs an 'EffectDesc' for applying one of default 'UnitOp's
 effectTypical :: UnitOpKey -> EffectDesc
 effectTypical key = Free $ TypicalUnitOp key $ Pure ()
