d1a0a56bfcede589e07bf1ae7d37575a228b4411
Anton Bukov <bukov.anton@gmail.com>
2020-04-23 14:36:54 +0500

    modify game ui

--- src/UI/Descriptions/GameUIDesc.hs
+++ src/UI/Descriptions/GameUIDesc.hs
@@ -12,13 +12,20 @@ import Prelude hiding
     map,
   )
 
+-- | A backend agnostic description of the game UI
 data UIDesc a b
   = GameUIDesc
-      { _map :: Map,
+      { -- | Game map
+        _map :: Map,
+        -- | Log
         _log :: Log,
+        -- | Stats
         _stats :: Stats,
-        _itemMenu :: ItemMenu a b,
+        -- | Equipped items
+        _equippedItems :: EquippedItems,
+        -- | Arrow press handler
         _onArrowPress :: Maybe (Keys.Arrows -> a -> b),
+        -- | Key press handler
         _onKeyPress :: Maybe (Keys.Keys -> a -> b)
       }
   deriving (Functor)
@@ -27,10 +34,9 @@ data Map = Map {_mapField :: [[Char]]}
 
 data Log = Log {_logRecords :: [String]}
 
-data Stats = Stats [String]
+data Stats = Stats {_statsRecords :: [(String, String)]}
 
-data ItemMenu a b = ItemMenu {_menuItems :: [(String, a -> b)]}
-  deriving (Functor)
+data EquippedItems = EquippedItems {_equippedItemsSlots :: [(String, Maybe String)]}
 
 type Builder a b = State (UIDesc a b)
 
@@ -40,65 +46,58 @@ makeLenses ''Map
 
 makeLenses ''Log
 
-makeLenses ''ItemMenu
+makeLenses ''Stats
 
-defaultMap :: Map
-defaultMap = Map [[]]
+makeLenses ''EquippedItems
 
-defaultLog :: Log
-defaultLog = Log []
+blankMap :: Map
+blankMap = Map [[]]
 
-defaultStats :: Stats
-defaultStats = Stats []
+blankLog :: Log
+blankLog = Log []
 
-defaultItemMenu :: ItemMenu a b
-defaultItemMenu = ItemMenu []
+blankStats :: Stats
+blankStats = Stats []
 
-defalutUIDesc :: UIDesc a b
-defalutUIDesc =
-  GameUIDesc defaultMap defaultLog defaultStats defaultItemMenu Nothing Nothing
+blankEquippedItems :: EquippedItems
+blankEquippedItems = EquippedItems []
 
+blankUIDesc :: UIDesc a b
+blankUIDesc =
+  GameUIDesc blankMap blankLog blankStats blankEquippedItems Nothing Nothing
+
+-- | Get an UIDesc from a builder
 makeUI :: Builder a b c -> UIDesc a b
-makeUI = flip execState defalutUIDesc
+makeUI = flip execState blankUIDesc
 
+-- | Set a map in game UI
 setMap :: [[Char]] -> Builder a b ()
 setMap m = modify $ set map (Map m)
 
+-- | Set log records
 setLog :: [String] -> Builder a b ()
 setLog newLog = modify $ set log (Log newLog)
 
+-- | Set an arrow press handler
 setArrowPress :: (Keys.Arrows -> a -> b) -> Builder a b ()
 setArrowPress f = modify $ set onArrowPress (Just f)
 
+-- | Set a key press handler
 setKeyPress :: (Keys.Keys -> a -> b) -> Builder a b ()
 setKeyPress f = modify $ set onKeyPress (Just f)
 
-setStats :: [String] -> Builder a b ()
+-- | Set stats records
+setStats :: [(String, String)] -> Builder a b ()
 setStats newStats = modify $ set stats (Stats newStats)
 
+-- | Add a log record to the end of the current log records
 addToLog :: String -> Builder a b ()
-addToLog item = modify $ over (log . logRecords) (item :)
+addToLog item = modify $ over (log . logRecords) (++ [item])
 
-addItem :: String -> (a -> b) -> Builder a b ()
-addItem item f = modify $ over (itemMenu . menuItems) ((item, f) :)
+-- | Add an equipped item to the end of the current equipped items list
+addEquippedItem :: String -> Maybe String -> Builder a b ()
+addEquippedItem slot item = modify $ over (equippedItems . equippedItemsSlots) (++ [(slot, item)])
 
-getMap :: UIDesc a b -> Map
-getMap = _map
-
-getLog :: UIDesc a b -> Log
-getLog = _log
-
-getStats :: UIDesc a b -> Stats
-getStats = _stats
-
-getItemMenu :: UIDesc a b -> ItemMenu a b
-getItemMenu = _itemMenu
-
-getOnArrowsKeyPress :: UIDesc a b -> Maybe (Keys.Arrows -> a -> b)
-getOnArrowsKeyPress = _onArrowPress
-
-getOnKeyPress :: UIDesc a b -> Maybe (Keys.Keys -> a -> b)
-getOnKeyPress = _onKeyPress
-
-getField :: Map -> [[Char]]
-getField = _mapField
+-- | Set equipped items records
+setEquippedItems :: [(String, Maybe String)] -> Builder a b ()
+setEquippedItems items = modify $ set equippedItems (EquippedItems items)
