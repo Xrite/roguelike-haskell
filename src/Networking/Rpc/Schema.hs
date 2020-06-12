fb7aef8a16b6a52ff74371e0e0ab9f727e015831
Vladilav Yepifanov <evn1999@yandex.ru>
2020-06-12 04:40:26 +0300

    finished basic schema

--- src/Networking/Rpc/Schema.hs
+++ src/Networking/Rpc/Schema.hs
@@ -1,60 +1,74 @@
-{-# language DataKinds             #-}
-{-# language DeriveAnyClass        #-}
-{-# language DeriveGeneric         #-}
-{-# language DuplicateRecordFields #-}
-{-# language FlexibleContexts      #-}
-{-# language FlexibleInstances     #-}
-{-# language MultiParamTypeClasses #-}
-{-# language PolyKinds             #-}
-{-# language TemplateHaskell       #-}
-{-# language TypeFamilies          #-}
-{-# language TypeOperators         #-}
-{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE PartialTypeSignatures #-}
-{-# language OverloadedStrings     #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Networking.Rpc.Schema where
 
-import RIO.FilePath ((</>), (<.>))
 import Data.Text as T
 import GHC.Generics
+import RIO.FilePath ((<.>), (</>))
 
+import Data.ByteString (ByteString)
+import Data.Int (Int32)
 import Mu.Quasi.GRpc
 import Mu.Schema
-import Data.Int (Int32)
-import Data.ByteString (ByteString)
 
 grpc "NetSchema" (++ "Servicer") $ "protos" </> "roguelike" <.> "proto"
 
-newtype SessionId
-  = SessionId { id :: Int32 }
+newtype SessionId =
+  SessionId
+    { id :: Int32
+    }
   deriving ( Generic
            , ToSchema NetSchema "SessionId"
            , FromSchema NetSchema "SessionId"
            )
 
-data SessionInfo
-  = SessionInfo
+data SessionInfo =
+  SessionInfo
     { name :: T.Text
     , id :: Maybe SessionId
-    } deriving ( Generic
-               , ToSchema NetSchema "SessionInfo"
-               , FromSchema NetSchema "SessionInfo"
-               )
+    }
+  deriving ( Generic
+           , ToSchema NetSchema "SessionInfo"
+           , FromSchema NetSchema "SessionInfo"
+           )
 
 data State
   = NOT_RUNNING
   | RUNNING
-  deriving ( Generic
-           , ToSchema NetSchema "State"
-           , FromSchema NetSchema "State"
-           )
+  deriving (Generic, ToSchema NetSchema "State", FromSchema NetSchema "State")
 
-data SessionState
-  = SessionState
+data SessionState =
+  SessionState
     { state :: Maybe State
     , content :: ByteString
-    } deriving ( Generic
-               , ToSchema NetSchema "SessionState"
-               , FromSchema NetSchema "SessionState"
-               )
+    }
+  deriving ( Generic
+           , ToSchema NetSchema "SessionState"
+           , FromSchema NetSchema "SessionState"
+           )
+
+data Empty =
+  Empty
+  deriving (Generic, ToSchema NetSchema "Empty", FromSchema NetSchema "Empty")
+
+newtype SessionsList =
+  SessionsList
+    { sessions :: [SessionInfo]
+    }
+  deriving ( Generic
+           , ToSchema NetSchema "SessionsList"
+           , FromSchema NetSchema "SessionsList"
+           )
